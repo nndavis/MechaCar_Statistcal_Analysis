fe5a9b6ae6b320099345ed7faacab8ca874f73fb
NoahKoala <104074135+nndavis@users.noreply.github.com>
2022-10-18 11:21:49 -0500

    deliv3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,8 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension_coildf <- read.csv('Suspension_Coil.csv', check.names = F,stringsAsFactors=F)
 total_summary <- Suspension_coildf %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 lot_summary <- Suspension_coildf %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+t.test((Suspension_coildf$PSI), mu=mean(1500))
+t.test(PSI ~ (subset=Manufacturing_Lot=='Lot1'), data=Suspension_coildf, mu=mean(1500))
+t.test(PSI ~ (subset=Manufacturing_Lot=='Lot2'), data=Suspension_coildf, mu=mean(1500))
+t.test(PSI ~ (subset=Manufacturing_Lot=='Lot3'), data=Suspension_coildf, mu=mean(1500))
